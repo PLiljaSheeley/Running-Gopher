90694799442bc096ced27b4ffe519a45a16ebf07
Peter Lilja-Sheeley <pliljasheeley@gmail.com>
2016-12-14 20:40:02 -0600

    added javascript for email list.

--- client/client.js
+++ client/client.js
@@ -1,1 +1,18 @@
-undefined
+var app = angular.module("app", []);
+
+app.controller("MainController", ["$scope", "$http", function($scope, $http) {
+
+  $scope.emailList = [];
+
+  $scope.getEmail = function() {
+    $http.get("/email").then(function(response) {
+      $scope.email = {};
+      $scope.emailArray = response.data;
+    });
+  };
+  $scope.getEmails();
+  $scope.add = function(email){
+    $http.post('/add', email).then($scope.getEmails());
+  }
+
+}]);
