30acd3a023dabf374b81370e3057e834fd3e46da
Peter Lilja-Sheeley <pliljasheeley@gmail.com>
2017-02-09 21:58:20 -0600

    angular show/hide function broken, css needed, formatting needed, way to access database for admin needed

--- client/client.js
+++ client/client.js
@@ -3,16 +3,67 @@ var app = angular.module("app", []);
 app.controller("MainController", ["$scope", "$http", function($scope, $http) {
 
   $scope.emailList = [];
-
-  $scope.getEmail = function() {
-    $http.get("/email").then(function(response) {
-      $scope.email = {};
-      $scope.emailArray = response.data;
-    });
-  };
-  $scope.getEmails();
-  $scope.add = function(email){
-    $http.post('/add', email);
-  }
+  $scope.orderHide = true;
+  $scope.aboutHide = false;
+  $scope.pricingHide = false;
+  $scope.contactHide = false;
+	$scope.showOrder = function(){
+	  $scope.orderHide = !$scope.orderHide;
+	  if($scope.aboutHide === true){
+	    $scope.aboutHide = !$scope.aboutHide
+	  }
+	   if($scope.pricingHide === true){
+	    $scope.pricingHide = !$scope.pricingHide
+	  }
+	  if($scope.contactHide === true){
+	    $scope.contactHide = !$scope.contactHide
+	  }
+	}
+	$scope.showAbout = function(){
+	  $scope.aboutHide = !$scope.aboutHide;
+	  if($scope.orderHide === true){
+	      $scope.orderHide = !$scope.orderHide
+	  }
+	   if($scope.pricingHide === true){
+	    $scope.pricingHide = !$scope.pricingHide
+	  }
+	  if($scope.contactHide === true){
+	    $scope.contactHide = !$scope.contactHide
+	  }
+	}
+	$scope.showPricing = function(){
+	  $scope.pricingHide = !$scope.pricingHide;
+	  if($scope.orderHide === true){
+	      $scope.orderHide = !$scope.orderHide
+	  }
+	   if($scope.aboutHide === true){
+	    $scope.aboutHide = !$scope.aboutHide
+	  }
+	  if($scope.contactHide === true){
+	    $scope.contactHide = !$scope.contactHide
+	  }
+	}
+	$scope.showContact = function(){
+	  $scope.contactHide = !$scope.contactHide;
+	  if($scope.orderHide === true){
+	      $scope.orderHide = !$scope.orderHide
+	  }
+	   if($scope.aboutHide === true){
+	    $scope.aboutHide = !$scope.aboutHide
+	  }
+	  if($scope.pricingHide === true){
+	    $scope.pricingHide = !$scope.pricingHide
+	  }
+	}
+  // $scope.getEmail = function() {
+  //   $http.get("/email").then(function(response) {
+  //     $scope.email = {};
+  //     $scope.emailArray = response.data;
+  //   });
+  // };
+  // $scope.getEmails();
+  // $scope.add = function(email){
+  //   $http.post('/add', email);
+  // }
 
 }]);
--- server/db/connection.js
+++ server/db/connection.js
@@ -10,3 +10,17 @@ if (process.env.DATABASE_URL){
   console.log('local var');
   connectionString = //TODO: postgres://localhost:5432/database name;
 }
+
+function initializeDB(){
+  return new Promise(function(resolve, reject) {
+  pg.connect(connectionString, function(err, client, done){
+    console.log('connected to pg');
+    if(err){
+      console.log('Error connecting to DB!', err);
+      reject(Error(err));
+      process.exit(1);
+
+    } else {
+
+      var listTable = 'CREATE TABLE IF NOT EXISTS emailList (' +
+      'email varchar(255) NOT NULL);'
